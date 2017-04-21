8c215c6a767831b303577690da2004b75571e9f8
metalmount <sokunbitaiwo82@gmail.com>
2017-04-21 13:28:44 +0100

    Add authentication

--- server.js
+++ server.js
@@ -3,10 +3,99 @@ const express =require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const dbConfig = require('./db.js');
+
 var db
 
-app.use(bodyParser.urlencoded({extended: true}))
+
+// Configuring Passport
+var passport = require('passport');
+var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+var exphbs = require('express-handlebars');
+var expressValidator = require('express-validator');
+var flash = require('connect-flash');
+var session = require('express-session');
+var localStrategy = require('passport-local').Strategy;
+
+var mongoose = require('mongoose');
+
+var routes =require('./routes/index');
+var users = require('./routes/users');
+
+
+//app.use(express.static("public"));
+//app.use(bodyParser.urlencoded({extended: true}))
+
+// Middleware
+app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'ejs');
+app.use(expressSession({secret: 'mySecretKey'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+//set static  folder
+app.use(express.static( path.join(__dirname, 'public')));
+
+
+//database connection
+mongoose.connect(dbConfig.url);
+
+
+// set expression session
+
+app.use(session({
+	secret:'secret',
+	saveUnintialized: true,
+	resave: true
+}));
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Express Validator
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+//connect flash
+app.use(flash());
+
+
+// global variable
+app.use(function(req, res, next) {
+  res.locals.user = req.session.user;
+  next();
+});
+
+app.use(function(req,res,next){
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
+
+// routing
+app.use('/',routes);
+app.use('/users',users);
+
 
 MongoClient.connect('mongodb://metalmount:metalmount%4082@ds163940.mlab.com:63940/notice-board',(err,database)=>{
   if(err) return console.log(err)
@@ -16,38 +105,47 @@ MongoClient.connect('mongodb://metalmount:metalmount%4082@ds163940.mlab.com:6394
 })
 });
 
-app.get('/',(req,res) =>{
-  res.render('index.ejs');  
-});
+// app.get('/',(req,res) =>{
+//   res.render('register.ejs');  
+// });
 
-app.get('/general', (req,res)=>{
-	db.collection('announcements').find().toArray((err,result)=>{
-		res.render('general.ejs',{announcements:result})
-	})
-})
 
-app.get('/fellow', (req,res)=>{
-	db.collection('announcements').find().toArray((err,result)=>{
-		res.render('fellow.ejs',{announcements:result})
-	})
-})
 
-app.get('/staff', (req,res)=>{
-	db.collection('announcements').find().toArray((err,result)=>{
-		res.render('staff.ejs',{announcements:result})
-	})
-})
 
-app.post('/general',(req,res)=>{
-	db.collection('announcements').save(req.body,(err,result)=>{
-	  if(err) return console.log(err)	
+
+// app.post('/general',(req,res)=>{
+// 	db.collection('announcements').save(req.body,(err,result)=>{
+// 	  if(err) return console.log(err)	
 	
-	  console.log("saved to the database");
-	  db.collection('announcements').find().toArray((err,results)=>{
-	  	console.log(results)
-	  })
-	  res.redirect('/general');
-	})
+// 	  console.log("saved to the database");
+// 	  db.collection('announcements').find().toArray((err,results)=>{
+// 	  	console.log(results)
+// 	  })
+// 	  res.redirect('/general');
+// 	})
 	
-})
-
+// })
+// app.post('/general',(req,res)=>{
+// 	db.collection('announcements').save(req.body,(err,result)=>{
+// 	  if(err) return console.log(err)	
+	
+// 	  console.log("saved to the database");
+// 	  db.collection('announcements').find().toArray((err,results)=>{
+// 	  	console.log(results)
+// 	  })
+// 	  res.redirect('/general');
+// 	})
+	
+// })
+// app.post('/general',(req,res)=>{
+// 	db.collection('announcements').save(req.body,(err,result)=>{
+// 	  if(err) return console.log(err)	
+	
+// 	  console.log("saved to the database");
+// 	  db.collection('announcements').find().toArray((err,results)=>{
+// 	  	console.log(results)
+// 	  })
+// 	  res.redirect('/general');
+// 	})
+	
+// })
